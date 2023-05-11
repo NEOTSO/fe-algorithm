cfbc83226739388e50aaa41ee24caf370df065c6
TSO <coding.tso@gmail.com>
2023-05-11 11:36:20 +0800

    feat: 20.valid-parentheses

---
+++ 20.valid-parentheses.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+function isValid(s) {
+    if (s.length % 2 === 1) return false;
+    const stack = [];
+    for (let i = 0; i < s.length; i++) {
+        const c = s[i];
+        if ("(" === c || "[" === c || "{" === c) {
+            stack.push(c);
+        } else {
+            const t = stack[stack.length - 1];
+            if (
+                ("(" === t && ")" === c) ||
+                ("[" === t && "]" === c) ||
+                ("{" === t && "}" === c)
+            ) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+}
